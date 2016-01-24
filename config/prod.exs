29ba3af6e0844cc59044c0cfe1eac0b1377c3e58
stanmartineau <stan.martineau@gmail.com>
2016-01-23 20:11:39 -0700

    Use production config from Heroku ENV variables

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,20 @@ use Mix.Config
 # which you typically run after static files are built.
 config :brunch_bros_phoenix, BrunchBrosPhoenix.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "guarded-garden-66180.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+
+#database config
+config :brunch_bros_phoenix, BrunchBrosPhoenix.Repo,
+    adapter: Ecto.Adapters.Postgress,
+    url: System.get_env("DATABASE_URL"),
+    pool_size: 20
 
 # ## SSL Support
 #
@@ -62,4 +71,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
