13bee405403093643f86db7bd8a90d214f156dd2
stanmartineau <stan.martineau@gmail.com>
2016-01-24 14:58:45 -0700

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule BrunchBrosPhoenix.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
-     {:ecto, "~> 1.1"}]
+     {:ecto, "~> 1.1.2"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
